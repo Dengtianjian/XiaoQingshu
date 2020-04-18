742c3b6f851178ba0e3d709d6ab171e5158a8738
DengTianjian <mail@isdtj.com>
2020-04-18 12:29:40 +0800

    增加页面
    增加 用户编辑资料 页面

---
+++ miniprogram/pages/my/edit_profile/edit_profile.js
@@ -0,0 +1,46 @@
+// miniprogram/pages/my/edit_profile/edit_profile.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    avatar: "",
+  },
+
+  changeAvatar() {
+    wx.chooseImage({
+      count: 1,
+    }).then((res) => {
+      let tempFile = res["tempFilePaths"][0];
+      console.log(this.data.avatar);
+      if(this.data.avatar==""){
+        this.animate(
+          ".change-avatar-overlay-preview",
+          [
+            {
+              opacity: 0,
+              width: 0,
+            },
+            {
+              opacity: 1,
+              width: "40%",
+            },
+          ],
+          500
+        );
+      }
+      this.setData({
+        avatar: tempFile,
+      });
+
+    });
+  },
+  pewviewAvatar() {
+    if (this.data.avatar == "") {
+      return;
+    }
+    wx.previewImage({
+      urls: [this.data.avatar],
+    });
+  },
+});
