65493fd0c42ff37448fe5f830119bbed184865af
DengTianjian <mail@isdtj.com>
2020-05-09 23:55:44 +0800

    cloud函数库增加callFun函数
    原本cfunction的替代

--- miniprogram/source/js/cloud.js
+++ miniprogram/source/js/cloud.js
@@ -30,6 +30,29 @@ export default {
         });
     });
   },
+  callFun(name, module, data) {
+    return new Promise(async (resolve, reject) => {
+      await wx.cloud
+        .callFunction({
+          name,
+          data: {
+            module,
+            ...data,
+          },
+        })
+        .then((res) => {
+          let result = res.result;
+          if (result.error == 200) {
+            resolve(result.data);
+          } else {
+            reject(result);
+          }
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  },
   DB() {
     return wx.cloud.database();
   },
