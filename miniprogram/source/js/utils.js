efdeb099309366848ef2dd05d330cd2d81bb39c3
DengTianjian <mail@isdtj.com>
2020-05-09 23:54:06 +0800

    优化格式化日期函数

--- miniprogram/source/js/utils.js
+++ miniprogram/source/js/utils.js
@@ -5,8 +5,9 @@ export default {
     let month = date.getMonth() + 1;
     month = month > 9 ? month : `0${month}`;
     let day = date.getDate();
+    day=day>9?day:`0${day}`;
     let hours = date.getHours();
-    hours = hours.length > 9 ? hours : `0${hours}`;
+    hours = hours > 9 ? hours : `0${hours}`;
     let minutes = date.getMinutes();
     minutes = minutes > 9 ? minutes : `0${minutes}`;
     let seconds = date.getSeconds();
