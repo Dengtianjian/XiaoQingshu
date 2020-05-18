f19b14f0afdb3fece8e002ae0b3748c670927693
DengTianjian <mail@isdtj.com>
2020-05-18 22:26:03 +0800

    优化弹窗组件
    增加porp round是否是圆角

--- miniprogram/components/popup/popup.js
+++ miniprogram/components/popup/popup.js
@@ -10,42 +10,62 @@ Component({
     position: {
       type: String,
       observer(e) {
+        let roundCss="";
         switch (e) {
           case "center":
+            if(this.data.round){
+              roundCss="border-radius:30rpx;";
+            }
             this.setData({
-              positionStyle:
-                "top:0;left:0;right:0;bottom:0;margin:auto;border-radius:30rpx;width:690rpx;height:400rpx;",
+              style:
+                "top:0;left:0;right:0;bottom:0;margin:auto;width:690rpx;height:400rpx;"+roundCss,
             });
             break;
           case "top":
+            if(this.data.round){
+              roundCss="border-radius:0 0 30rpx 30rpx;";
+            }
             this.setData({
-              positionStyle:
-                "top:0;min-height:60rpx;width:100%;border-radius:0 0 30rpx 30rpx;",
+              style:
+                "top:0;min-height:60rpx;width:100%;"+roundCss,
             });
             break;
           case "right":
+            if(this.data.round){
+              roundCss="border-radius:30rpx 0 0 30rpx;";
+            }
             this.setData({
-              positionStyle: "right:0;height:100%;min-width:140rpx;",
+              style: "right:0;height:100%;min-width:140rpx;"+roundCss,
             });
             break;
           case "left":
+            if(this.data.round){
+              roundCss="border-radius:0 30rpx 30rpx 0;";
+            }
             this.setData({
-              positionStyle: "left:0;height:100%;min-width:140rpx;",
+              style: "left:0;height:100%;min-width:140rpx;"+roundCss,
             });
             break;
           case "bottom":
+            if(this.data.round){
+              roundCss="border-radius:30rpx 30rpx 0 0;";
+            }
             this.setData({
-              positionStyle:
-                "bottom:0;min-height:60rpx;width:100%;border-radius:30rpx 30rpx 0 0;",
+              style:
+                "bottom:0;min-height:60rpx;width:100%;"+roundCss,
             });
             break;
         }
-      },
+      }
     },
     customStyle: {
       type: String,
     },
     overlay:{
+      type:Boolean,
+      value:true
+    },
+    round:{
       type:Boolean,
       value:true
     }
@@ -55,7 +75,11 @@ Component({
    * 组件的初始数据
    */
   data: {
-    positionStyle: "",
+    style:""
+  },
+
+  attache(){
+
   },
 
   /**
