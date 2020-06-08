4f7bc9a30400201288ed17d5d17fb2e666bdfa4d
DengTianjian <mail@isdtj.com>
2020-06-08 21:51:07 +0800

    修复个人空间页获取的帖子问题

--- miniprogram/pages/my/space/space.js
+++ miniprogram/pages/my/space/space.js
@@ -85,31 +85,6 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {},
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {},
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {},
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {},
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {},
-
-  /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
@@ -135,7 +110,7 @@ Page({
     Cloud.cfunction("Post","getPostByUser",{
       page:this.PostPagination.getPage(),
       limit:this.PostPagination.limit,
-      userid:this.data.userInfo['_userid']
+      userid:this.data.userInfo['_id']
     }).then(res=>{
       if(res.length<this.PostPagination.limit){
         this.PostPagination.setFinished(true);
