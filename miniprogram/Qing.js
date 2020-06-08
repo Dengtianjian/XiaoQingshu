92ad08164ea42eba9cf0da410a46f09fbeabc74e
DengTianjian <mail@isdtj.com>
2020-06-09 01:50:26 +0800

    增加消息提醒库引入

--- miniprogram/Qing.js
+++ miniprogram/Qing.js
@@ -2,10 +2,12 @@ import Prompt from "./source/js/prompt";
 import Pagination from "./source/js/pagination";
 import Utils from "./source/js/utils";
 import Cloud from "./source/js/cloud";
+import Notification from "./functions/notification";
 
 module.exports = {
   Prompt,
   Utils,
   Cloud,
-  Pagination
+  Pagination,
+  Notification
 };
